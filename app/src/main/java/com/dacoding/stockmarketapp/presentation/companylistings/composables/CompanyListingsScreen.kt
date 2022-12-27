83a0dc91c0d10cc16d78ae6096c1cbc19affa029
Denny <andreevden1803@gmail.com>
2022-12-27 22:05:44 +0300

    Company listing UI Item and Screen commit

---
+++ app/src/main/java/com/dacoding/stockmarketapp/presentation/companylistings/composables/CompanyListingsScreen.kt
@@ -0,0 +1,87 @@
+package com.dacoding.stockmarketapp.presentation.companylistings.composables
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.dacoding.stockmarketapp.presentation.companylistings.CompanyListingsEvent
+import com.dacoding.stockmarketapp.presentation.companylistings.CompanyListingsViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Composable
+@Destination(start = true)
+fun CompanyListingsScreen(
+    navigator: DestinationsNavigator,
+    viewModel: CompanyListingsViewModel = hiltViewModel()
+) {
+    val swipeRefreshState = rememberSwipeRefreshState(
+        isRefreshing = viewModel.state.isRefreshing
+    )
+    val state = viewModel.state
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = state.searchQuery,
+            onValueChange = {
+                viewModel.onEvent(
+                    CompanyListingsEvent
+                        .OnSearchQueryChange(it)
+                )
+            },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            placeholder = {
+                Text(text = "Search")
+            },
+            maxLines = 1,
+            singleLine = true,
+            leadingIcon = {
+                Icon(imageVector = Icons.Outlined.Search, contentDescription = null)
+            }
+        )
+        SwipeRefresh(
+            state = swipeRefreshState,
+            onRefresh = {
+                viewModel.onEvent(CompanyListingsEvent.Refresh)
+            }
+        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(state.companies.size) { i ->
+                    val company = state.companies[i]
+                    CompanyItem(
+                        company = company,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                TODO("Navigate to detail screen")
+                            }
+                            .padding(16.dp)
+                    )
+                    if (i < state.companies.size) {
+                        Divider(
+                            modifier = Modifier.padding(
+                                horizontal = 16.dp
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}

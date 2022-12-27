03a30617df9abcc0cd3c34143ed159b8c74f843e
Denny <andreevden1803@gmail.com>
2022-12-27 19:59:59 +0300

    Entity, Mapper, Dao

---
+++ app/src/main/java/com/dacoding/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -0,0 +1,20 @@
+package com.dacoding.stockmarketapp.data.mapper
+
+import com.dacoding.stockmarketapp.data.local.CompanyListingEntity
+import com.dacoding.stockmarketapp.domain.model.CompanyListing
+
+fun CompanyListingEntity.toCompanyListing(): CompanyListing {
+    return CompanyListing(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
+
+fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
+    return CompanyListingEntity(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}

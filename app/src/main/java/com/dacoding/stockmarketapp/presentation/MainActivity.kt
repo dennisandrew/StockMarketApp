0ad30c7e6b0a0ab575bce7889c1181479057da25
Denny <andreevden1803@gmail.com>
2022-12-27 18:48:07 +0300

    Initial commit

---
+++ app/src/main/java/com/dacoding/stockmarketapp/presentation/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.dacoding.stockmarketapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.dacoding.stockmarketapp.presentation.ui.theme.StockMarketAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StockMarketAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
